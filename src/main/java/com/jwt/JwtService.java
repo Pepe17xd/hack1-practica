16653b2f5312dfd0665eab62f71446481da23dab
dayroncueva-ops <dayron.cueva@utec.edu.pe>
2026-05-03 09:36:01 -0500

    Update JwtService.java

--- src/main/java/com/jwt/JwtService.java
+++ src/main/java/com/jwt/JwtService.java
@@ -1,5 +1,27 @@
 package com.jwt;
 
+@Service
 public class JwtService {
+    private final String SECRET = "clave_secreta_super_segura";
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("role", user.getRole().name())
+                .claim("branch", user.getBranch())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
 
 }
