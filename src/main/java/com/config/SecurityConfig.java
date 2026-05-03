30db01ff2bc5d412cc1d41bc6f023694db89b17c
dayroncueva-ops <dayron.cueva@utec.edu.pe>
2026-05-03 09:37:39 -0500

    Update SecurityConfig.java

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -1,5 +1,21 @@
 package com.example.oreo;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
