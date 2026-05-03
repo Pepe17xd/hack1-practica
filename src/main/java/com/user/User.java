129df4e4dec5a14cdd419b6f91e0df0933e48cb0
dayroncueva-ops <dayron.cueva@utec.edu.pe>
2026-05-03 09:36:25 -0500

    Update User.java

--- src/main/java/com/user/User.java
+++ src/main/java/com/user/User.java
@@ -1,5 +1,28 @@
 package com.user;
 
-public class User {
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
+    private String username;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String branch; // null si es CENTRAL
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public String getUsername() { return username; }
 }
