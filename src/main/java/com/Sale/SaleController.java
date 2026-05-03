3d3c084d373588915fd612e82b0f9cf99cb21895
dayroncueva-ops <dayron.cueva@utec.edu.pe>
2026-05-03 09:34:50 -0500

    Update SaleController.java

--- src/main/java/com/Sale/SaleController.java
+++ src/main/java/com/Sale/SaleController.java
@@ -1,5 +1,13 @@
 package com.Sale;
 
 public class SaleController {
+  @PostMapping
+  public ResponseEntity<?> create(@RequestBody Sale sale,
+                               Authentication authentication) {
 
+  User user = (User) authentication.getPrincipal();
+
+  return ResponseEntity.status(201)
+            .body(saleService.createSale(sale, user));
+  }
 }
