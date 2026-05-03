26c7a8ab9ecc64fb78e2965be8755eb24732bd17
dayroncueva-ops <dayron.cueva@utec.edu.pe>
2026-05-03 09:35:08 -0500

    Update SaleService.java

--- src/main/java/com/Sale/SaleService.java
+++ src/main/java/com/Sale/SaleService.java
@@ -1,5 +1,21 @@
 package com.Sale;
 
+@Service
 public class SaleService {
 
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public Sale createSale(Sale sale, User user) {
+
+        if (user.getRole() == Role.ROLE_BRANCH) {
+
+            if (!sale.getBranch().equals(user.getBranch())) {
+                throw new RuntimeException("403 - No puedes vender en otra sucursal");
+            }
+        }
+
+        sale.setCreatedBy(user.getUsername());
+        return saleRepository.save(sale);
+    }
 }
